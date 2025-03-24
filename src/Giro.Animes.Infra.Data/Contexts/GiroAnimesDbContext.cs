3629305e50db8a9b290c739f28103ae13d0c9fdc
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-24 19:54:12 -0300

    Adicionar GiroAnimesDbContext para gerenciamento de entidades
    Este commit introduz a classe `GiroAnimesDbContext`, que herda de `DbContext`. A classe define várias propriedades `DbSet` para gerenciar entidades como `User`, `Account`, `Avatar`, `Settings` e `Language`. Também inclui um construtor que aceita `DbContextOptions` e um método `OnModelCreating` sobrescrito, onde as configurações das entidades são aplicadas.

---
+++ src/Giro.Animes.Infra.Data/Contexts/GiroAnimesDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Giro.Animes.Domain.Entities;
+using Giro.Animes.Infra.Data.Configurations.Types;
+using Microsoft.EntityFrameworkCore;
+
+namespace Giro.Animes.Infra.Data.Contexts
+{
+    public class GiroAnimesDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Avatar> Avatars { get; set; }
+        public DbSet<Settings> Settings { get; set; }
+        public DbSet<Language> Languages { get; set; }
+
+        public GiroAnimesDbContext(DbContextOptions<GiroAnimesDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new UserEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new AccountEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new AvatarEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new LanguageEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new SettingsEntityTypeConfiguration());
+        }
+    }
+}
