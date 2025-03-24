7f33beb9f5bdb79b90993820f4ec3266c34bf813
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-24 19:53:18 -0300

    Adiciona configurações de entidades para o EF Core
    As mudanças incluem a adição de configurações de tipo de entidade para várias entidades de domínio em uma aplicação C# usando Entity Framework Core. As novas classes de configuração, como `AccountEntityTypeConfiguration`, `AvatarEntityTypeConfiguration`, `EntityBaseTypeConfiguration`, `LanguageEntityTypeConfiguration`, `SettingsEntityTypeConfiguration` e `UserEntityTypeConfiguration`, definem como as entidades são mapeadas para o banco de dados, incluindo nomes de tabelas, relacionamentos e configurações de propriedades. As classes especificam relacionamentos entre entidades, propriedades obrigatórias e valores padrão, além de introduzir o uso de objetos de valor para propriedades como `Email`, `Password`, `Status` e `Plan`, melhorando a expressividade e encapsulamento do modelo de domínio.

---
+++ src/Giro.Animes.Infra.Data/Configurations/Types/Base/EntityBaseTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Giro.Animes.Domain.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Giro.Animes.Infra.Data.Configurations.Types.Base
+{
+    internal class EntityBaseTypeConfiguration<Derivate> : IEntityTypeConfiguration<Derivate> where Derivate : EntityBase
+    {
+        public virtual void Configure(EntityTypeBuilder<Derivate> builder)
+        {
+            builder.HasKey(entityBase => entityBase.Id);
+            builder.Ignore(entityBase => entityBase.IsValid);
+            builder.HasQueryFilter(entityBase => entityBase.DeletionDate != null);
+
+            builder.Property(entityBase => entityBase.CreationDate).ValueGeneratedOnAdd().HasColumnType("TIMESTAMP").HasDefaultValueSql("CURRENT_TIMESTAMP");
+            builder.Property(entityBase => entityBase.UpdateDate).ValueGeneratedOnAddOrUpdate().HasColumnType("TIMESTAMP").HasDefaultValueSql("CURRENT_TIMESTAMP");
+            builder.Property(entityBase => entityBase.DeletionDate).HasColumnType("TIMESTAMP").HasDefaultValueSql(null).IsRequired(false);
+        }
+    }
+}
---
+++ src/Giro.Animes.Infra.Data/Configurations/Types/SettingsEntityTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Giro.Animes.Domain.Entities;
+using Giro.Animes.Domain.Enums;
+using Giro.Animes.Infra.Data.Configurations.Types.Base;
+using Giro.Animes.Infra.Data.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Giro.Animes.Infra.Data.Configurations.Types
+{
+    internal class SettingsEntityTypeConfiguration : EntityBaseTypeConfiguration<Settings>
+    {
+        public override void Configure(EntityTypeBuilder<Settings> builder)
+        {
+            base.Configure(builder);
+
+            builder.ToTable(Tables.Common.SETTINGS, Schemas.COMMON);
+            builder.HasOne(settings => settings.User).WithOne(user => user.Settings).IsRequired(true);
+            builder.HasOne(settings => settings.Language).WithMany(language => language.Settings).HasForeignKey(settings => settings.LanguageId).IsRequired(true);
+            builder.Navigation(settings => settings.Language);
+            builder.Navigation(settings => settings.User);
+
+            builder.Property(settings => settings.Theme).IsRequired(true).HasConversion(theme => theme.Value, value => UserTheme.FromValue(value)).HasDefaultValue(UserTheme.Light);
+            builder.Property(settings => settings.EnableApplicationNotifications).IsRequired().HasDefaultValue(true).HasDefaultValue(true);
+            builder.Property(settings => settings.EnableEmailNotifications).IsRequired().HasDefaultValue(false).HasDefaultValue(false);
+        }
+    }
+}
