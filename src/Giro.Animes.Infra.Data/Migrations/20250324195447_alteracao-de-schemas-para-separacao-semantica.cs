333e4b40dc2259bd88afa859c465169d03e2d1d5
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-24 19:54:26 -0300

    Separação de esquemas e melhorias na estrutura do banco
    Implementa novas tabelas em esquemas distintos ("management", "content", "common") para melhor organização. Atualiza métodos de migração para criação e reversão de tabelas, incluindo valores padrão. Reflete o estado atual do esquema no snapshot do modelo.

---
+++ src/Giro.Animes.Infra.Data/Migrations/20250324195447_alteracao-de-schemas-para-separacao-semantica.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Giro.Animes.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class alteracaodeschemasparaseparacaosemantica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "management");
+
+            migrationBuilder.EnsureSchema(
+                name: "content");
+
+            migrationBuilder.EnsureSchema(
+                name: "common");
+
+            migrationBuilder.CreateTable(
+                name: "languages",
+                schema: "common",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Code = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: false),
+                    NativeName = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreationDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdateDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    DeletionDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_languages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "management",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreationDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdateDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    DeletionDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                schema: "management",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    IsConfirmed = table.Column<bool>(type: "boolean", nullable: true, defaultValue: false),
+                    Password_Value = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    Password_Salt = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Plan = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdateDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    DeletionDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_accounts_users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "management",
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "settings",
+                schema: "common",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EnableApplicationNotifications = table.Column<bool>(type: "boolean", nullable: false, defaultValue: true),
+                    EnableEmailNotifications = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    Theme = table.Column<int>(type: "integer", nullable: false),
+                    LanguageId = table.Column<long>(type: "bigint", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    LanguageId1 = table.Column<long>(type: "bigint", nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdateDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    DeletionDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_settings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_settings_languages_LanguageId",
+                        column: x => x.LanguageId,
+                        principalSchema: "common",
+                        principalTable: "languages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_settings_languages_LanguageId1",
+                        column: x => x.LanguageId1,
+                        principalSchema: "common",
+                        principalTable: "languages",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_settings_users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "management",
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "avatars",
+                schema: "content",
+                columns: table => new
+                {
+                    Url = table.Column<string>(type: "text", nullable: false),
+                    Extension = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AccountId = table.Column<long>(type: "bigint", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdateDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    DeletionDate = table.Column<DateTime>(type: "TIMESTAMP", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_avatars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_avatars_accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalSchema: "management",
+                        principalTable: "accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_accounts_UserId",
+                schema: "management",
+                table: "accounts",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_avatars_AccountId",
+                schema: "content",
+                table: "avatars",
+                column: "AccountId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_settings_LanguageId",
+                schema: "common",
+                table: "settings",
+                column: "LanguageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_settings_LanguageId1",
+                schema: "common",
+                table: "settings",
+                column: "LanguageId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_settings_UserId",
+                schema: "common",
+                table: "settings",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "avatars",
+                schema: "content");
+
+            migrationBuilder.DropTable(
+                name: "settings",
+                schema: "common");
+
+            migrationBuilder.DropTable(
+                name: "accounts",
+                schema: "management");
+
+            migrationBuilder.DropTable(
+                name: "languages",
+                schema: "common");
+
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "management");
+        }
+    }
+}
---
+++ src/Giro.Animes.Infra.Data/Migrations/20250324200715_atualizacao-default-value-settings.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Giro.Animes.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class atualizacaodefaultvaluesettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Theme",
+                schema: "common",
+                table: "settings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Theme",
+                schema: "common",
+                table: "settings",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 1);
+        }
+    }
+}
