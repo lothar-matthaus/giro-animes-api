b4fcfd5a627a58c7a053928d96cf80e4bd0c9395
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-24 19:52:43 -0300

    Adiciona classe Injection para configuração do DbContext
    Implementa a classe estática Injection com o método AddGiroAnimesDbContext para configurar a injeção de dependência do GiroAnimesDbContext usando PostgreSQL.

---
+++ src/Giro.Animes.Infra.Data/Configurations/IoC/Injection.cs
@@ -0,0 +1,23 @@
+﻿using Giro.Animes.Infra.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Infra.Data.Configurations.IoC
+{
+    public static class Injection
+    {
+        public static void AddGiroAnimesDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<GiroAnimesDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("GiroAnimesDb"));
+            });
+        }
+    }
+}
