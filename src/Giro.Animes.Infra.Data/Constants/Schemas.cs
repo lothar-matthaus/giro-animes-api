2ed4d2311e6d9e80302684181db28e2d487568de
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-24 19:53:55 -0300

    Adiciona classes Schemas e Tables para constantes de banco
    Este commit introduz duas novas classes, `Schemas` e `Tables`, no namespace `Giro.Animes.Infra.Data.Constants`. A classe `Schemas` define quatro valores constantes representando diferentes esquemas: `MANAGEMENT`, `CONTENT`, `COMMON` e `LOCALIZATION`. A classe `Tables` contém classes aninhadas para `Management`, `Content` e `Common`, cada uma definindo seus próprios valores constantes para várias tabelas de banco de dados, como `ACCOUNTS`, `USERS`, `AVATARS`, `SETTINGS` e `LANGUAGES`. Essa estrutura organiza constantes relacionadas a esquemas e tabelas de banco de dados, melhorando a clareza e a manutenibilidade do código.

---
+++ src/Giro.Animes.Infra.Data/Constants/Schemas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Infra.Data.Constants
+{
+    public static class Schemas
+    {
+        public const string MANAGEMENT = "management";
+        public const string CONTENT = "content";
+        public const string COMMON = "common";
+        public const string LOCALIZATION = "localization";
+    }
+}
---
+++ src/Giro.Animes.Infra.Data/Constants/Tables.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Infra.Data.Constants
+{
+    public static class Tables
+    {
+        public static class Management
+        {
+            public const string ACCOUNTS = "accounts";
+            public const string USERS = "users";
+        }
+        public static class Content
+        {
+            public const string AVATARS = "avatars";
+        }
+
+        public static class Common
+        {
+            public const string SETTINGS = "settings";
+            public const string LANGUAGES = "languages";
+        }
+    }
+}
