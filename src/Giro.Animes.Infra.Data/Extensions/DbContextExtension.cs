c7a6c0a5abece093353d388f51937f1b03bacab0
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-25 01:14:33 -0300

    Adicionar extensão DbContext para conversão em snake_case
    Implementa a classe estática DbContextExtension com o método
    ToSnakeCase, que converte nomes de tabelas, colunas e chaves
    para o formato snake_case, melhorando a consistência e
    manutenibilidade do esquema do banco de dados.

---
+++ src/Giro.Animes.Infra.Data/Extensions/DbContextExtension.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Giro.Animes.Infra.Data.Extensions
+{
+    internal static class DbContextExtension
+    {
+        public static void ToSnakeCase(this DbContext dbContext, ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                // Configura o nome da tabela para usar snake_case
+                entity.SetTableName(FormatToSnakeCase(entity.GetTableName()));
+
+                // Configura o nome das colunas para usar snake_case
+                foreach (var property in entity.GetProperties())
+                {
+                    property.SetColumnName(FormatToSnakeCase(property.GetColumnName()));
+                }
+
+                // Configura o nome das chaves estrangeiras para usar snake_case
+                foreach (var key in entity.GetForeignKeys())
+                {
+                    key.SetConstraintName(FormatToSnakeCase(key.GetConstraintName()));
+                }
+
+                // Configura os nomes das chaves primárias para usar snake_case
+                foreach (var key in entity.GetKeys())
+                {
+                    key.SetName(FormatToSnakeCase(key.GetName()));
+                }
+            }
+
+            string FormatToSnakeCase(string name)
+            {
+                if (string.IsNullOrEmpty(name))
+                    return name;
+
+                var regex = new Regex(@"([a-z0-9])([A-Z])");
+                var snakeCaseName = regex.Replace(name, "$1_$2").ToLowerInvariant();
+
+                Console.WriteLine($"Alterado [{name}] para [{snakeCaseName}]");
+                return snakeCaseName;
+            }
+        }
+    }
+}
