27a3ab1cc3a3ca36f008468e732aaa281c6e5009
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-25 13:23:19 -0300

    Adicionar classes BiographyAuthors e GenreDescription
    Duas novas classes foram adicionadas ao namespace
    Giro.Animes.Domain.Entities.Joint para gerenciar relacionamentos
    entre autores, gêneros e descrições, com propriedades adequadas
    e construtores sem parâmetros.

---
+++ src/Giro.Animes.Domain/Entities/Joint/BiographyAuthors.cs
@@ -0,0 +1,19 @@
+﻿using Giro.Animes.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Domain.Entities.Joint
+{
+    public class BiographyAuthors
+    {
+        public long DescriptionId { get; private set; }
+        public Description Description { get; private set; }
+        public long AuthorId { get; private set; }
+        public Author Author { get; private set; }
+
+        public BiographyAuthors() { }
+    }
+}
---
+++ src/Giro.Animes.Domain/Entities/Joint/GenreDescription.cs
@@ -0,0 +1,18 @@
+﻿using Giro.Animes.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Domain.Entities.Joint
+{
+    public class GenreDescription
+    {
+        public long GenreId { get; private set; }
+        public Genre Genre { get; set; }
+        public long DescriptionId { get; private set; }
+        public Description Description { get; private set; }
+        public GenreDescription() { }
+    }
+}
