3d260d24cf4bf193369fff120abb9a2be45ad335
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-25 01:13:54 -0300

    Remover diretivas using e adicionar classes de esquema
    Removidas diretivas `using` em `Schemas.cs` e `Tables.cs`.
    Adicionadas classes estáticas `Schemas` e `Tables`, com constantes
    para gerenciamento e conteúdo, além de uma nova classe `Common`
    com várias constantes de tabelas comuns.

--- src/Giro.Animes.Infra.Data/Constants/Tables.cs
+++ src/Giro.Animes.Infra.Data/Constants/Tables.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Giro.Animes.Infra.Data.Constants
+﻿namespace Giro.Animes.Infra.Data.Constants
 {
     public static class Tables
     {
@@ -16,12 +10,21 @@ namespace Giro.Animes.Infra.Data.Constants
         public static class Content
         {
             public const string AVATARS = "avatars";
+            public const string COVERS = "covers";
         }
 
         public static class Common
         {
             public const string SETTINGS = "settings";
+            public const string AUTHORS = "authors";
             public const string LANGUAGES = "languages";
+            public const string DESCRIPTIONS = "descriptions";
+            public const string BIOGRAPHY_AUTHORS = "biography_authors";
+            public const string DESCRIPTION_GENRES = "description_genres";
+            public const string GENRES = "genres";
+            public const string TITLE_GENRES = "title_genres";
+            public const string TITLES = "titles";
+
         }
     }
 }
