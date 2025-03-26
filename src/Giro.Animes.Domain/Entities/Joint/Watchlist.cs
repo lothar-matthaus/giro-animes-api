eedf594778d60642ea3778742311020bdd273cb0
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-26 01:56:28 -0300

    Remover classes obsoletas e adicionar novas entidades
    Removidas as classes `BiographyAuthors` e `GenreDescription`.
    Adicionadas as classes `Watchlist` e `Works` com propriedades
    e documentação apropriadas.

---
+++ src/Giro.Animes.Domain/Entities/Joint/Watchlist.cs
@@ -0,0 +1,20 @@
+﻿using Giro.Animes.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Domain.Entities.Joint
+{
+    public class Watchlist : EntityBase
+    {
+        public long AccountId { get; private set; }
+        public Account Account { get; private set; }
+
+        public long AnimeId { get; private set; }
+        public Anime Anime { get; private set; }
+
+        public Watchlist() { }
+    }
+}
---
+++ src/Giro.Animes.Domain/Entities/Joint/Works.cs
@@ -0,0 +1,41 @@
+﻿using Giro.Animes.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Domain.Entities.Joint
+{
+    /// <summary>
+    /// Representa a junção entre autores e animes
+    /// </summary>
+    public class Works : EntityBase
+    {
+        /// <summary>
+        /// Identificador do autor
+        /// </summary>
+        public long AuthorId { get; set; }
+
+        /// <summary>
+        /// Identificador do anime
+        /// </summary>
+        public long AnimeId { get; set; }
+
+        /// <summary>
+        /// Navegação para o autor
+        /// </summary>
+        public Author Author { get; set; }
+
+        /// <summary>
+        /// Navegação para o anime
+        /// </summary>
+        public Anime Anime { get; set; }
+
+        /// <summary>
+        /// Construtor padrão 
+        /// </summary>
+        public Works() { }
+
+    }
+}
