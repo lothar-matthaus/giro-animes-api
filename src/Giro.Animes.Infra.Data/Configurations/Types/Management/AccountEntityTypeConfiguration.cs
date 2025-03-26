d9fc65153aaf75c9c36736fa6c484d9c8540fda7
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-26 01:58:11 -0300

    Adiciona configurações de entidades para Account, Settings e User
    As mudanças introduzem novas configurações de tipo de entidade para
    `Account`, `Settings` e `User` no namespace
    `Giro.Animes.Infra.Data.Configurations.Types.Management`. Cada classe
    de configuração herda de `EntityBaseTypeConfiguration<T>` e
    sobrescreve o método `Configure` para definir as propriedades,
    relacionamentos e mapeamentos de tabela no banco de dados.
    Essas alterações melhoram a estrutura e integridade do banco de
    dados, garantindo que os relacionamentos e propriedades estejam
    definidos corretamente para o modelo de dados da aplicação.

---
+++ src/Giro.Animes.Infra.Data/Configurations/Types/Management/AccountEntityTypeConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Giro.Animes.Domain.Entities;
+using Giro.Animes.Domain.Entities.Joint;
+using Giro.Animes.Domain.Enums;
+using Giro.Animes.Domain.ValueObjects;
+using Giro.Animes.Infra.Data.Configurations.Types.Base;
+using Giro.Animes.Infra.Data.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Giro.Animes.Infra.Data.Configurations.Types.Management
+{
+    internal class AccountEntityTypeConfiguration : EntityBaseTypeConfiguration<Account>
+    {
+        public override void Configure(EntityTypeBuilder<Account> builder)
+        {
+            base.Configure(builder);
+            builder.ToTable(Tables.Management.ACCOUNTS, Schemas.MANAGEMENT);
+            builder.HasOne(account => account.User).WithOne(user => user.Account).IsRequired(true);
+            builder.HasOne(account => account.Avatar).WithOne(avatar => avatar.Account).HasForeignKey<Avatar>(avatar => avatar.AccountId).IsRequired(true);
+            builder.HasMany(account => account.Watchlist).WithMany(anime => anime.Accounts).UsingEntity<Watchlist>(
+            Tables.Content.WATCHLIST, // Nome da tabela de junção
+            join => join.HasOne(watch => watch.Anime)
+                  .WithMany()
+                  .HasForeignKey(join => join.AnimeId) // Nome da coluna FK para Genre
+                  .OnDelete(DeleteBehavior.Cascade),
+            join => join.HasOne(watch => watch.Account)
+                  .WithMany()
+                  .HasForeignKey(join => join.AccountId) // Nome da coluna FK para Description
+                  .OnDelete(DeleteBehavior.Cascade),
+            join =>
+            {
+                join.ToTable(Tables.Content.WATCHLIST, Schemas.CONTENT);
+            });
+
+            builder.OwnsOne(account => account.Email, email =>
+            {
+                email.Property(email => email.Value).HasColumnName(nameof(Account.Email)).HasMaxLength(100).IsRequired(true);
+                email.Property(email => email.IsConfirmed).HasColumnName(nameof(Account.Email.IsConfirmed)).IsRequired(true).HasDefaultValue(false);
+            });
+
+            builder.OwnsOne(account => account.Password, password =>
+            {
+                password.Property(password => password.Value).HasColumnName(nameof(Password)).IsRequired(true).HasMaxLength(256);
+                password.Property(password => password.Salt).HasColumnName(nameof(Password.Salt)).IsRequired(true).HasMaxLength(256);
+                password.Ignore(password => password.PlainTextConfirm);
+                password.Ignore(password => password.PlainText);
+            });
+
+            builder.Property(account => account.Status).IsRequired(true).HasConversion(status => status.Value, value => AccountStatus.FromValue(value));
+            builder.Property(account => account.Plan).IsRequired(true).HasConversion(plan => plan.Value, value => AccountPlan.FromValue(value));
+        }
+    }
+}
---
+++ src/Giro.Animes.Infra.Data/Configurations/Types/Management/SettingsEntityTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Giro.Animes.Domain.Entities;
+using Giro.Animes.Domain.Enums;
+using Giro.Animes.Infra.Data.Configurations.Types.Base;
+using Giro.Animes.Infra.Data.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Giro.Animes.Infra.Data.Configurations.Types.Management
+{
+    internal class SettingsEntityTypeConfiguration : EntityBaseTypeConfiguration<Settings>
+    {
+        public override void Configure(EntityTypeBuilder<Settings> builder)
+        {
+            base.Configure(builder);
+
+            builder.ToTable(Tables.Management.SETTINGS, Schemas.MANAGEMENT);
+            builder.HasOne(settings => settings.Account).WithOne(acc => acc.Settings).HasForeignKey<Settings>(set => set.AccountId).IsRequired(true);
+            builder.HasOne(settings => settings.Language).WithMany(language => language.Settings).HasForeignKey(settings => settings.LanguageId).IsRequired(true);
+            builder.Navigation(settings => settings.Language);
+            builder.Navigation(settings => settings.Account);
+
+            builder.Property(settings => settings.Theme).IsRequired(true).HasConversion(theme => theme.Value, value => UserTheme.FromValue(value)).HasDefaultValue(UserTheme.Light);
+            builder.Property(settings => settings.EnableApplicationNotifications).IsRequired().HasDefaultValue(true).HasDefaultValue(true);
+            builder.Property(settings => settings.EnableEmailNotifications).IsRequired().HasDefaultValue(false).HasDefaultValue(false);
+        }
+    }
+}
