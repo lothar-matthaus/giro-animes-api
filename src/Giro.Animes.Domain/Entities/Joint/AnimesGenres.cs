42fdd03573223fe22ada0718dc96812a8d45ded6
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-26 20:09:34 -0300

    Adicionar classe AnimesGenres para relacionar Anime e Gênero
    A nova classe `AnimesGenres` foi criada no namespace
    `Giro.Animes.Domain.Entities.Joint`. Ela representa a relação
    entre um Anime e um Gênero, incluindo propriedades para
    `AnimeId`, `GenreId` e referências às entidades `Anime` e
    `Genre`. A classe herda de `EntityBase` e possui um
    construtor sem parâmetros.

---
+++ src/Giro.Animes.Domain/Entities/Joint/AnimesGenres.cs
@@ -0,0 +1,40 @@
+﻿using Giro.Animes.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Domain.Entities.Joint
+{
+    /// <summary>
+    /// Represents the relationship between an Anime and a Genre.
+    /// </summary>
+    public class AnimesGenres : EntityBase
+    {
+        /// <summary>
+        /// Gets the identifier of the Anime.
+        /// </summary>
+        public long AnimeId { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the identifier of the Genre.
+        /// </summary>
+        public long GenreId { get; set; }
+
+        /// <summary>
+        /// Gets the Anime associated with this relationship.
+        /// </summary>
+        public Anime Anime { get; private set; }
+
+        /// <summary>
+        /// Gets the Genre associated with this relationship.
+        /// </summary>
+        public Genre Genre { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AnimesGenres"/> class.
+        /// </summary>
+        public AnimesGenres() { }
+    }
+}
