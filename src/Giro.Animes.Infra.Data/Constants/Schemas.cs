f4ca3888e474970444bdba8bc59fc3f7e8a09386
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-26 01:58:42 -0300

    Reorganiza constantes em Schemas e Tables
    Removido `LOCALIZATION` e adicionado `MISC` em Schemas.cs.
    Reestruturada a classe `Tables` com novas constantes em `Content`
    e `Management`, além da adição da classe vazia `Misc`.

--- src/Giro.Animes.Infra.Data/Constants/Schemas.cs
+++ src/Giro.Animes.Infra.Data/Constants/Schemas.cs
@@ -5,6 +5,6 @@
         public const string MANAGEMENT = "management";
         public const string CONTENT = "content";
         public const string COMMON = "common";
-        public const string LOCALIZATION = "localization";
+        public const string MISC = "misc";
     }
 }
--- src/Giro.Animes.Infra.Data/Constants/Tables.cs
+++ src/Giro.Animes.Infra.Data/Constants/Tables.cs
@@ -2,29 +2,42 @@
 {
     public static class Tables
     {
+        public static class Common
+        {
+            public const string AUTHORS = "authors";
+            public const string GENRES = "genres";
+            public const string LANGUAGES = "languages";
+            public const string STUDIOS = "studios";
+            public const string BIOGRAPHIES = "biographies";
+        }
+
+        public static class Content
+        {
+            public const string ANIMES = "animes";
+            public const string ANIME_SCREENSHOTS = "anime_screenshots";
+            public const string ANIME_TITLES = "anime_titles";
+            public const string AVATARS = "avatars";
+            public const string COVERS = "covers";
+            public const string EPISODES = "episodes";
+            public const string EPISODE_FILES = "episode_files";
+            public const string EPISODE_TITLES = "episode_titles";
+            public const string GENRE_DESCRIPTIONS = "genre_descriptions";
+            public const string GENRE_TITLES = "genre_titles";
+            public const string LOGOS = "logos";
+            public const string SINOPSES = "sinopses";
+            public const string AUTHOR_WORKS = "author_works";
+            public const string WATCHLIST = "watchlist";
+        }
+
         public static class Management
         {
             public const string ACCOUNTS = "accounts";
             public const string USERS = "users";
-        }
-        public static class Content
-        {
-            public const string AVATARS = "avatars";
-            public const string COVERS = "covers";
-        }
-
-        public static class Common
-        {
             public const string SETTINGS = "settings";
-            public const string AUTHORS = "authors";
-            public const string LANGUAGES = "languages";
-            public const string DESCRIPTIONS = "descriptions";
-            public const string BIOGRAPHY_AUTHORS = "biography_authors";
-            public const string GENRE_DESCRIPTIONS = "genre_descriptions";
-            public const string GENRES = "genres";
-            public const string GENRE_TITLES = "genre_titles";
-            public const string TITLES = "titles";
+        }
 
+        public static class Misc
+        {
         }
     }
 }
