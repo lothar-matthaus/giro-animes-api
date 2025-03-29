5c4c40df5b75cdd3643246174b94d73023926ffd
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-29 14:14:09 -0300

    Adiciona classes de idiomas e limpa diretivas não utilizadas
    As mudanças incluem a adição das classes `EpisodeLanguages` e
    `SettingsAnimesLanguage`, ambas herdando de `EntityBase` e
    contendo propriedades relevantes. As classes `Watchlist` e
    `Works` foram mantidas, mas as diretivas de using foram
    removidas para simplificar o código e melhorar o modelo
    de domínio com novas relações.

---
+++ src/Giro.Animes.Domain/Entities/Joint/EpisodeLanguages.cs
@@ -0,0 +1,13 @@
+﻿using Giro.Animes.Domain.Entities.Base;
+
+namespace Giro.Animes.Domain.Entities.Joint
+{
+    public class EpisodeLanguages : EntityBase
+    {
+        public long EpisodeId { get; private set; }
+        public long LanguageId { get; private set; }
+
+        public Language Language { get; private set; }
+        public Episode Episode { get; private set; }
+    }
+}
---
+++ src/Giro.Animes.Domain/Entities/Joint/SettingsAnimesLanguage.cs
@@ -0,0 +1,12 @@
+﻿using Giro.Animes.Domain.Entities.Base;
+
+namespace Giro.Animes.Domain.Entities.Joint
+{
+    public class SettingsAnimesLanguage : EntityBase
+    {
+        public long LanguageId { get; private set; }
+        public long SettingsId { get; private set; }
+        public Settings Settings { get; set; }
+        public Language Language { get; set; }
+    }
+}
