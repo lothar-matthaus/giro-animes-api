0d4c095b271c63f1b7bc3db73f3784e2ea4229f4
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-29 14:18:02 -0300

    Adiciona suporte a IApplicationUser nos DbContexts
    Adiciona campo _user em GiroAnimesReadDbContext e GiroAnimesWriteDbContext,
    inicializando via IServiceProvider. Modifica OnModelCreating para
    passar _user para várias configurações de entidade. Remove
    OnConfiguring, sugerindo maior uso de injeção de dependência.

--- src/Giro.Animes.Infra.Data/Contexts/GiroAnimesReadDbContext.cs
+++ src/Giro.Animes.Infra.Data/Contexts/GiroAnimesReadDbContext.cs
@@ -1,22 +1,22 @@
-﻿using Giro.Animes.Domain.Entities;
-using Giro.Animes.Infra.Data.Configurations.Types.Common;
+﻿using Giro.Animes.Infra.Data.Configurations.Types.Common;
 using Giro.Animes.Infra.Data.Configurations.Types.Content;
 using Giro.Animes.Infra.Data.Configurations.Types.Management;
+using Giro.Animes.Infra.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Giro.Animes.Infra.Data.Contexts
 {
     public class GiroAnimesReadDbContext : DbContext
     {
-        public GiroAnimesReadDbContext(DbContextOptions<GiroAnimesReadDbContext> options) : base(options)
+        private readonly IApplicationUser _user;
+        public GiroAnimesReadDbContext(DbContextOptions<GiroAnimesReadDbContext> options, IServiceProvider serviceProvider) : base(options)
         {
-
+            _user = serviceProvider.GetRequiredService<IApplicationUser>();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
-
             modelBuilder.ApplyConfiguration(new UserEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new AccountEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new AvatarEntityTypeConfiguration());
@@ -26,27 +26,20 @@ namespace Giro.Animes.Infra.Data.Contexts
             modelBuilder.ApplyConfiguration(new AuthorEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new EpisodeEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new AnimeEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new StudioEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new AnimeScreenshotEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new StudioEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new AnimeTitleEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new CoverEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new BiographyEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new AnimeTitleEntityTypeConfiguration(_user));
+            modelBuilder.ApplyConfiguration(new CoverEntityTypeConfiguration(_user));
+            modelBuilder.ApplyConfiguration(new BiographyEntityTypeConfiguration(_user));
             modelBuilder.ApplyConfiguration(new EpisodeFileEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new EpisodeTitleEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new GenreDescriptionEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new GenreTitleEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new EpisodeTitleEntityTypeConfiguration(_user));
+            modelBuilder.ApplyConfiguration(new GenreDescriptionEntityTypeConfiguration(_user));
+            modelBuilder.ApplyConfiguration(new GenreTitleEntityTypeConfiguration(_user));
             modelBuilder.ApplyConfiguration(new LogoEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new AnimeSinopseEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new AnimeSinopseEntityTypeConfiguration(_user));
             modelBuilder.ApplyConfiguration(new RatingEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new EpisodeSinopseEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new EpisodeSinopseEntityTypeConfiguration(_user));
             // this.ToSnakeCase(modelBuilder);
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
         }
     }
 }
