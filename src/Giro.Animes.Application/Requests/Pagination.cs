61d8560157893dc7844b8abe6bd6cbb3d9fff134
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-29 14:12:45 -0300

    Adicionar classe Pagination com interface IPagination
    Este commit introduz a nova classe `Pagination` no namespace
    `Giro.Animes.Application.Requests`, que implementa a interface
    `IPagination`. A classe contém as propriedades `Page`,
    `RowsPerPage` e `Count`, todas do tipo `int`. Também foi
    adicionada uma diretiva using para incluir a interface necessária.

---
+++ src/Giro.Animes.Application/Requests/Pagination.cs
@@ -0,0 +1,13 @@
+﻿using Giro.Animes.Domain.Interfaces.Pagination;
+
+namespace Giro.Animes.Application.Requests
+{
+    public class Pagination : IPagination
+    {
+        public int Page { get; set; }
+
+        public int RowsPerPage { get; set; }
+
+        public int Count { get; set; }
+    }
+}
