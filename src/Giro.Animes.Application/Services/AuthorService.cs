6e2c957f78852791dbc239da6b44cffe74aee924
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-29 14:13:11 -0300

    Adicionar AuthorService e ApplicationServiceBase
    Implementa AuthorService para gerenciar autores e adiciona
    ApplicationServiceBase como classe base para serviços de aplicação.

---
+++ src/Giro.Animes.Application/Services/AuthorService.cs
@@ -0,0 +1,34 @@
+﻿using Giro.Animes.Application.DTOs;
+using Giro.Animes.Application.Interfaces.Services;
+using Giro.Animes.Application.Mappers;
+using Giro.Animes.Application.Services.Base;
+using Giro.Animes.Domain.Entities;
+using Giro.Animes.Domain.Interfaces.Pagination;
+using Giro.Animes.Domain.Interfaces.Services;
+using Giro.Animes.Infra.Interfaces;
+using System.Reflection.Metadata;
+
+namespace Giro.Animes.Application.Services
+{
+    public class AuthorService : ApplicationServiceBase<IAuthorDomainService>, IAuthorService
+    {
+        public AuthorService(IApplicationUser applicationUser, IAuthorDomainService domainService) : base(applicationUser, domainService)
+        {
+        }
+
+        public async Task<IEnumerable<AuthorDTO>> GetAllAuthorsPagedAsync(IPagination param)
+        {
+            IEnumerable<Author> authors = await _domainService.GetAllAuthorsPagedAsync(param);
+            IEnumerable<AuthorDTO> result = authors.Map();
+
+            return result;
+        }
+
+        public async Task<AuthorDTO> GetAuthorByIdAsync(long id)
+        {
+            Author author = await _domainService.GetAuthorByIdAsync(id);
+            AuthorDTO authorDTO = author.Map();
+            return authorDTO;
+        }
+    }
+}
---
+++ src/Giro.Animes.Application/Services/Base/ApplicationServiceBase.cs
@@ -0,0 +1,17 @@
+﻿using Giro.Animes.Application.Interfaces.Services.Base;
+using Giro.Animes.Domain.Interfaces.Services.Base;
+using Giro.Animes.Infra.Interfaces;
+
+namespace Giro.Animes.Application.Services.Base
+{
+    public class ApplicationServiceBase<TDomainService> : IApplicationServiceBase where TDomainService : IDomainServiceBase
+    {
+        protected readonly IApplicationUser _applicationUser;
+        protected readonly TDomainService _domainService;
+        public ApplicationServiceBase(IApplicationUser applicationUser, TDomainService domainService)
+        {
+            _applicationUser = applicationUser;
+            _domainService = domainService;
+        }
+    }
+}
