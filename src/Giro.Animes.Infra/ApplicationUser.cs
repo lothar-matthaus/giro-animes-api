138bc1053a8951f5f00f98ca286d1af8b7bf66e7
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-29 14:16:48 -0300

    Adiciona classe ApplicationUser e atualiza IApplicationUser
    Implementa a classe ApplicationUser para gerenciar informações do usuário a partir do contexto HTTP. Atualiza o arquivo .csproj com novas referências de pacotes e adiciona propriedades à interface IApplicationUser.

---
+++ src/Giro.Animes.Infra/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Giro.Animes.Domain.Enums;
+using Giro.Animes.Infra.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Giro.Animes.Infra
+{
+    public class ApplicationUser(IHttpContextAccessor context) : IApplicationUser
+    {
+        private static UserRole GetUserRole(string role)
+        {
+            return role switch
+            {
+                "Admin" => UserRole.Admin,
+                "User" => UserRole.User,
+                "Guest" => UserRole.Guest,
+                "Moderator" => UserRole.Moderator,
+                "Publisher" => UserRole.Publisher,
+                _ => UserRole.Guest
+            };
+        }
+
+        public long Id { get; private set; } = long.Parse(context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Sid)).Select(cl => cl.Value).FirstOrDefault() ?? "0");
+        public string Nome { get; private set; } = context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Name)).Select(cl => cl.Value).FirstOrDefault() ?? "Guest";
+        public string Email { get; private set; } = context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Email)).Select(cl => cl.Value).FirstOrDefault() ?? "";
+        public UserRole Role { get; private set; } = GetUserRole(context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Role)).Select(cl => cl.Value).FirstOrDefault() ?? "");
+        public string[] Language { get; private set; } = context.HttpContext?.Request.Headers["Accept-Language"].FirstOrDefault()?.Split(',', StringSplitOptions.TrimEntries) ?? ["en-US"];
+    }
+}
---
+++ src/Giro.Animes.Infra/Interfaces/IApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Giro.Animes.Domain.Enums;
+
+namespace Giro.Animes.Infra.Interfaces
+{
+    public interface IApplicationUser
+    {
+        public long Id { get; }
+        public string Nome { get; }
+        public string Email { get; }
+        public UserRole Role { get; }
+        public string[] Language { get; }
+    }
+}
