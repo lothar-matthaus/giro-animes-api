c2f18c4b631364b63b92b046b24389cc2751a443
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-29 14:15:05 -0300

    Refatorar IReadBaseRepository para suporte a paginação
    Modifica a interface `IReadBaseRepository` no arquivo
    `IReadBaseRepository.cs`. O método `GetAllAsync` foi removido
    e substituído por `GetAllPagedAsync`, que agora aceita um
    parâmetro `IPagination` e um `CancellationToken` para
    recuperação paginada de entidades. Um novo using para
    `Giro.Animes.Domain.Interfaces.Pagination` foi adicionado.

--- src/Giro.Animes.Domain/Interfaces/Repositories/Read/Base/IReadBaseRepository.cs
+++ src/Giro.Animes.Domain/Interfaces/Repositories/Read/Base/IReadBaseRepository.cs
@@ -1,10 +1,11 @@
 ﻿using Giro.Animes.Domain.Entities.Base;
+using Giro.Animes.Domain.Interfaces.Pagination;
 
 namespace Giro.Animes.Domain.Interfaces.Repositories.Read.Base
 {
     public interface IReadBaseRepository<TEntity> where TEntity : EntityBase, new()
     {
         Task<TEntity> GetByIdAsync(long id, CancellationToken cancellationToken);
-        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+        Task<IEnumerable<TEntity>> GetAllPagedAsync(IPagination param, CancellationToken cancellationToken);
     }
 }
