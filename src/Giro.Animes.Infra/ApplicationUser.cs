ebaab88344fa41ede7a25706f75a7b64982fff64
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-29 15:35:47 -0300

    Refatorar ApplicationUser para usar injeção de dependência
    A classe `ApplicationUser` foi refatorada para utilizar injeção de dependência para o `IHttpContextAccessor`. O construtor agora aceita um `IServiceProvider` em vez de receber diretamente um `IHttpContextAccessor`, melhorando a separação de preocupações e facilitando os testes.
    
    As propriedades `Id`, `Nome`, `Email`, `Role` e `Language` foram modificadas para usar o campo `_context`, garantindo melhor encapsulamento e consistência no acesso ao contexto. O valor padrão da propriedade `Language` foi atualizado para inicializar um array de forma mais padrão, garantindo que o valor padrão seja `["en-US"]` caso nenhum valor seja encontrado nos cabeçalhos.

--- src/Giro.Animes.Infra/ApplicationUser.cs
+++ src/Giro.Animes.Infra/ApplicationUser.cs
@@ -1,12 +1,18 @@
 ﻿using Giro.Animes.Domain.Enums;
 using Giro.Animes.Infra.Interfaces;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using System.Security.Claims;
 
 namespace Giro.Animes.Infra
 {
-    public class ApplicationUser(IHttpContextAccessor context) : IApplicationUser
+    public class ApplicationUser : IApplicationUser
     {
+        private readonly IHttpContextAccessor _context;
+        public ApplicationUser(IServiceProvider serviceProvider)
+        {
+            _context = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+        }
         private static UserRole GetUserRole(string role)
         {
             return role switch
@@ -20,10 +26,10 @@ namespace Giro.Animes.Infra
             };
         }
 
-        public long Id { get; private set; } = long.Parse(context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Sid)).Select(cl => cl.Value).FirstOrDefault() ?? "0");
-        public string Nome { get; private set; } = context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Name)).Select(cl => cl.Value).FirstOrDefault() ?? "Guest";
-        public string Email { get; private set; } = context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Email)).Select(cl => cl.Value).FirstOrDefault() ?? "";
-        public UserRole Role { get; private set; } = GetUserRole(context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Role)).Select(cl => cl.Value).FirstOrDefault() ?? "");
-        public string[] Language { get; private set; } = context.HttpContext?.Request.Headers["Accept-Language"].FirstOrDefault()?.Split(',', StringSplitOptions.TrimEntries) ?? ["en-US"];
+        public long Id => long.Parse(_context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Sid)).Select(cl => cl.Value).FirstOrDefault() ?? "0");
+        public string Nome => _context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Name)).Select(cl => cl.Value).FirstOrDefault() ?? "Guest";
+        public string Email => _context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Email)).Select(cl => cl.Value).FirstOrDefault() ?? "";
+        public UserRole Role => GetUserRole(_context.HttpContext.User.Claims.Where(cl => cl.Type.Equals(ClaimTypes.Role)).Select(cl => cl.Value).FirstOrDefault() ?? "");
+        public string[] Language => _context.HttpContext?.Request.Headers["Accept-Language"].FirstOrDefault()?.Split(',', StringSplitOptions.TrimEntries) ?? ["en-US"];
     }
 }
