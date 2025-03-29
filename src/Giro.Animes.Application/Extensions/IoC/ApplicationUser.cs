3e127e347cbee3afbdcee58e5d40d86eae70cedd
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-29 14:11:44 -0300

    Atualiza ApplicationUser e adiciona serviços de configuração
    Modifica o arquivo `ApplicationUser.cs` para alterar o namespace e a implementação do método `AddApplicationUser`, que agora retorna um `IServiceCollection` e utiliza `Infra.ApplicationUser`.
    
    O arquivo `Services.cs` foi atualizado com novos using directives e adiciona os métodos `ConfigureApplicationServices` e `ConfigureDomainServices` para registrar `IAuthorService` e `IAuthorDomainService` no contêiner de injeção de dependência.

--- src/Giro.Animes.Application/Extensions/IoC/ApplicationUser.cs
+++ src/Giro.Animes.Application/Extensions/IoC/ApplicationUser.cs
@@ -1,18 +1,14 @@
-﻿using Giro.Animes.Application.Interfaces;
+﻿using Giro.Animes.Infra.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Giro.Animes.Application.Extensions.IoC
 {
     public static class ApplicationUser
     {
-        public static void AddApplicationUser(this IServiceCollection services)
+        public static IServiceCollection AddApplicationUser(this IServiceCollection services)
         {
-            services.AddScoped<IApplicationUser, Application.ApplicationUser>();
+            services.AddScoped<IApplicationUser, Infra.ApplicationUser>();
+            return services;
         }
     }
 }
---
+++ src/Giro.Animes.Application/Extensions/IoC/Services.cs
@@ -0,0 +1,21 @@
+﻿using Giro.Animes.Application.Interfaces.Services;
+using Giro.Animes.Application.Services;
+using Giro.Animes.Domain.Interfaces.Services;
+using Giro.Animes.Domain.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Giro.Animes.Application.Extensions.IoC
+{
+    public static class Services
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthorService, AuthorService>();
+        }
+
+        public static void ConfigureDomainServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthorDomainService, AuthorDomainService>();
+        }
+    }
+}
