fecc35c428dee70e257dd7d2b834a9b11dfc62b2
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-29 14:14:38 -0300

    Adicionar interface IPagination com propriedades de paginação
    Foi adicionada a interface `IPagination` no namespace
    `Giro.Animes.Domain.Interfaces.Pagination`, que contém as
    propriedades `Page`, `RowsPerPage` e `Count`, todas do tipo
    `int` com getters e setters.

---
+++ src/Giro.Animes.Domain/Interfaces/Pagination/IPagination.cs
@@ -0,0 +1,9 @@
+﻿namespace Giro.Animes.Domain.Interfaces.Pagination
+{
+    public interface IPagination
+    {
+        public int Page { get; set; }
+        public int RowsPerPage { get; set; }
+        public int Count { get; set; }
+    }
+}
