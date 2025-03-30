a89dc45a722c232799fdae50e60a38e91c33ebe2
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-30 20:11:49 -0300

    Adicionar interface IUserService com métodos assíncronos
    Este commit adiciona a nova interface `IUserService` no namespace `Giro.Animes.Application.Interfaces.Services`. A interface estende `IApplicationServiceBase` e inclui dois métodos assíncronos: `GetUserAndAccountByUserIdAsync`, que recupera um usuário e sua conta pelo ID do usuário, e `CreateUserAsync`, que cria um novo usuário com base em uma solicitação fornecida.

---
+++ src/Giro.Animes.Application/Interfaces/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Giro.Animes.Application.DTOs;
+using Giro.Animes.Application.Interfaces.Services.Base;
+using Giro.Animes.Application.Requests.User;
+
+namespace Giro.Animes.Application.Interfaces.Services
+{
+    public interface IUserService : IApplicationServiceBase
+    {
+        Task<UserDTO> GetUserAndAccountByUserIdAsync(long userId);
+        Task<UserDTO> CreateUserAsync(UserCreateRequest request);
+    }
+}
