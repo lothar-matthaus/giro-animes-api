bf896103ab77f57465fc2d80983695d322be1fe7
Lothar Matthaus <lothar1258@hotmail.com>
2025-03-30 20:11:41 -0300

    Adicionar extensões de armazenamento de arquivos e serviços
    Este commit introduz uma nova classe estática `FileStorageExtensions` no arquivo `FileStorageExtensions.cs`, que inclui o método `ReadAsBytes` para ler o conteúdo de um `IFormFile` como um array de bytes.
    
    Além disso, foram adicionadas várias novas registragens de serviços nos métodos `ConfigureApplicationServices` e `ConfigureDomainServices` no arquivo `Services.cs`, incluindo `IUserService`, `NotificationService`, `IUserDomainService`, `LanguageDomainService` e um `MediaDomainService` genérico para `Avatar`.

---
+++ src/Giro.Animes.Application/Extensions/FileStorageExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Application.Extensions
+{
+    public static class FileStorageExtensions
+    {
+        public static byte[] ReadAsBytes(this IFormFile file)
+        {
+            using (var stream = file.OpenReadStream())
+            {
+                byte[] bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, (int)stream.Length);
+                return bytes;
+            }
+        }
+    }
+}
--- src/Giro.Animes.Application/Extensions/IoC/Services.cs
+++ src/Giro.Animes.Application/Extensions/IoC/Services.cs
@@ -1,5 +1,8 @@
 ﻿using Giro.Animes.Application.Interfaces.Services;
 using Giro.Animes.Application.Services;
+using Giro.Animes.Domain.Entities;
+using Giro.Animes.Domain.Interfaces.Repositories.Read.Base;
+using Giro.Animes.Domain.Interfaces.Repositories.Write;
 using Giro.Animes.Domain.Interfaces.Services;
 using Giro.Animes.Domain.Services;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,11 +14,16 @@ namespace Giro.Animes.Application.Extensions.IoC
         public static void ConfigureApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<IAuthorService, AuthorService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<INotificationService, NotificationService>();
         }
 
         public static void ConfigureDomainServices(this IServiceCollection services)
         {
             services.AddScoped<IAuthorDomainService, AuthorDomainService>();
+            services.AddScoped<IUserDomainService, UserDomainService>();
+            services.AddScoped<ILanguageDomainService, LanguageDomainService>();
+            services.AddScoped<IMediaDomainService<Avatar>, MediaDomainService<IMediaWriteRepository<Avatar>, IMediaReadRepository<Avatar>, Avatar>>();
         }
     }
 }
