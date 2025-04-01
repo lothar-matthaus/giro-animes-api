c81762b7133edf45a2c476dfdf06241e018bc1ef
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-01 19:54:37 -0300

    Remover INotificationService e atualizar IUserDomainService
    O arquivo `INotificationService.cs` foi completamente removido, incluindo a definição da interface e seus métodos. No arquivo `IUserDomainService.cs`, o tipo de retorno do método `CreateUser` foi alterado de `Task<User>` para `Task<EntityResult<User>>`, indicando uma modificação na forma como o resultado do processo de criação de usuário é tratado, provavelmente para incluir informações adicionais sobre o sucesso ou falha da operação.

--- src/Giro.Animes.Domain/Interfaces/Services/IUserDomainService.cs
+++ src/Giro.Animes.Domain/Interfaces/Services/IUserDomainService.cs
@@ -1,11 +1,12 @@
 ﻿using Giro.Animes.Domain.Entities;
 using Giro.Animes.Domain.Interfaces.Services.Base;
+using Giro.Animes.Domain.ValueObjects;
 
 namespace Giro.Animes.Domain.Interfaces.Services
 {
     public interface IUserDomainService : IDomainServiceBase
     {
         Task<User> GetUserAndAccountById(long userId);
-        Task<User> CreateUser(User user);
+        Task<EntityResult<User>> CreateUser(User user);
     }
 }
