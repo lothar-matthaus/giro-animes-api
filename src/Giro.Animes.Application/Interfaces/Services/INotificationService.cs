28c7544f4a66cedba69bbb88615b4e48f1152cb5
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-01 19:51:55 -0300

    Adicionar serviço de notificação e interface correspondente
    Este commit introduz a interface `INotificationService` em `INotificationService.cs`, que define métodos para gerenciar notificações, incluindo adição e recuperação de notificações. A interface inclui métodos como `HasNotifications`, `AddNotification` (sobrecarregado para diferentes parâmetros) e `GetNotifications`.
    
    Além disso, uma nova classe `NotificationService` é implementada em `NotificationService.cs`, que mantém uma lista privada de notificações e fornece implementações para os métodos da interface. A classe inclui lógica para adicionar notificações individuais ou múltiplas, bem como verificar se há notificações presentes. O método `AddNotification` também permite adicionar uma notificação usando uma mensagem, contexto e propriedade, criando um novo objeto `Notification`.

---
+++ src/Giro.Animes.Application/Interfaces/Services/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using Giro.Animes.Domain.Interfaces.Services.Base;
+using Giro.Animes.Domain.ValueObjects;
+
+namespace Giro.Animes.Application.Interfaces.Services
+{
+    public interface INotificationService : IDomainServiceBase
+    {
+        bool HasNotifications();
+        Task AddNotification(Notification notification);
+        Task AddNotification(IEnumerable<Notification> notifications);
+        Task AddNotification(string message, string context, string property);
+        Task<IEnumerable<Notification>> GetNotifications();
+    }
+}
---
+++ src/Giro.Animes.Application/Services/NotificationService.cs
@@ -0,0 +1,44 @@
+﻿using Giro.Animes.Application.Interfaces.Services;
+using Giro.Animes.Domain.ValueObjects;
+
+namespace Giro.Animes.Application.Services
+{
+    public class NotificationService : INotificationService
+    {
+        private List<Notification> _notifications = new List<Notification>();
+
+        public Task AddNotification(Notification notification)
+        {
+            _notifications ??= new List<Notification>();
+            _notifications.Add(notification);
+
+            return Task.CompletedTask;
+        }
+
+        public Task AddNotification(IEnumerable<Notification> notifications)
+        {
+            _notifications ??= new List<Notification>();
+            _notifications.AddRange(notifications);
+
+            return Task.CompletedTask;
+        }
+
+        public Task AddNotification(string message, string context, string property)
+        {
+            _notifications ??= new List<Notification>();
+            _notifications.Add(Notification.Create(context, property, message));
+
+            return Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<Notification>> GetNotifications()
+        {
+            return Task.FromResult(_notifications.AsEnumerable());
+        }
+
+        public bool HasNotifications()
+        {
+            return _notifications.Any();
+        }
+    }
+}
