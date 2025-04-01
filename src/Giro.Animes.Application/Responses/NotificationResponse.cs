f363a530aff25a8ca0ce436b831c910df6ba07bf
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-01 19:52:48 -0300

    Remover namespaces não utilizados em NotificationResponse.cs
    O arquivo `NotificationResponse.cs` foi modificado para remover várias diretivas `using`. Os namespaces removidos incluem `System`, `System.Collections.Generic` e `System.Linq`. As diretivas `using` restantes são `Giro.Animes.Application.Responses.Base`, `Giro.Animes.Domain.ValueObjects`, `System.Net`, `System.Text` e `System.Threading.Tasks`. Essa mudança pode indicar uma refatoração do código para eliminar namespaces não utilizados ou para simplificar as importações, melhorando a clareza e o desempenho.

--- src/Giro.Animes.Application/Responses/NotificationResponse.cs
+++ src/Giro.Animes.Application/Responses/NotificationResponse.cs
@@ -1,11 +1,6 @@
 ﻿using Giro.Animes.Application.Responses.Base;
 using Giro.Animes.Domain.ValueObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Giro.Animes.Application.Responses
 {
