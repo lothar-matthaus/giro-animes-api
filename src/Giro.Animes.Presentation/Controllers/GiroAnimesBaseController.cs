161d3bbf587f45e30f6921e54bde42bd766d2e8d
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-04 00:10:47 -0300

    Remover métodos de resposta e adicionar filtro de API
    As mudanças em `GiroAnimesBaseController.cs` removem os métodos assíncronos `Ok<TDto>(TDto data)` e `Ok<TDto>(IEnumerable<TDto> items, Pagination pagination)`, que retornavam respostas detalhadas e paginadas, indicando uma mudança na forma como as respostas são tratadas na aplicação. Um novo diretório de uso para `Giro.Animes.Shared.Filters` foi adicionado.
    
    Em `StandardApiResponseFilter.cs`, uma nova classe `StandardApiResponseFilter` foi introduzida, implementando a interface `IActionFilter`. Esta classe contém os métodos `OnActionExecuting` e `OnActionExecuted`, onde o último modifica o resultado da ação com base no tipo de resultado retornado. Se o resultado for um `ObjectResult` contendo uma lista de `BaseDTO`, ele cria um `ListPagedResponse`. Se contiver um único `BaseDTO`, ele cria um `DetailResponse`. Caso contrário, o resultado é definido como nulo. Essa mudança padroniza o tratamento de respostas da API em toda a aplicação.

--- src/Giro.Animes.Presentation/Controllers/GiroAnimesBaseController.cs
+++ src/Giro.Animes.Presentation/Controllers/GiroAnimesBaseController.cs
@@ -3,6 +3,7 @@ using Giro.Animes.Application.Interfaces.Services.Base;
 using Giro.Animes.Application.Requests;
 using Giro.Animes.Application.Responses;
 using Giro.Animes.Application.Responses.Base;
+using Giro.Animes.Shared.Filters;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
@@ -17,32 +18,6 @@ namespace Giro.Animes.Presentation.Controllers
         public GiroAnimesBaseController(TApplicationService applicationService)
         {
             _applicationService = applicationService;
-        }
-
-        public async Task<DetailResponse<TDto>> Ok<TDto>(TDto data) where TDto : BaseDTO
-        {
-            return await Task.Run(() =>
-            {
-                if (data == null)
-                    return DetailResponse<TDto>.Create(true, HttpStatusCode.OK, "Não foi encontrado nenhum resultado.");
-
-                DetailResponse<TDto> response = DetailResponse<TDto>.Create(data, true, HttpStatusCode.OK, "O recurso foi encontrado com sucesso.");
-                return response;
-            });
-        }
-
-        public async Task<ListPagedResponse<TDto>> Ok<TDto>(IEnumerable<TDto> items, Pagination pagination)
-            where TDto : BaseDTO
-        {
-            return await Task.Run(() =>
-            {
-                if (items == null || !items.Any())
-                    return ListPagedResponse<TDto>.Create(pagination, HttpStatusCode.OK, "A consulta não encontrou nenhum resultado.");
-
-                ListPagedResponse<TDto> response = ListPagedResponse<TDto>.Create(items, pagination, true, HttpStatusCode.OK, "A consulta retornou uma lista de resultados.");
-
-                return response;
-            });
         }
     }
 }
---
+++ src/Giro.Animes.Presentation/Filters/StandardApiResponseFilter.cs
@@ -0,0 +1,45 @@
+﻿using Giro.Animes.Application.Custom;
+using Giro.Animes.Application.DTOs.Base;
+using Giro.Animes.Application.Interfaces.Enumerations;
+using Giro.Animes.Application.Responses;
+using Giro.Animes.Application.Responses.Base;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Shared.Filters
+{
+    public class StandardApiResponseFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // No action needed before the action executes
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.Result is ObjectResult objectResult)
+            {
+                if (objectResult.Value is IPagedEnumerable<BaseDTO> list)
+                {
+                    context.Result = new ObjectResult(ListPagedResponse<BaseDTO>
+                        .Create(list, true, HttpStatusCode.OK, "A consulta retornou resultados.", list.Page, list.RowsPerPage, list.TotalCount));
+                }
+
+                else if (objectResult.Value is BaseDTO dto)
+                {
+                    context.Result = new ObjectResult(DetailResponse<BaseDTO>.Create(dto, true, HttpStatusCode.OK, "A consulta retornou os detalhes."));
+                }
+                else
+                {
+                    context.Result = new ObjectResult(null);
+                }
+            }
+        }
+    }
+}
