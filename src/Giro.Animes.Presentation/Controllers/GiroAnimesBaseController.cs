dee5940295a046b3cf025661a67f4ede1b622045
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-06 14:14:31 -0300

    Atualiza GiroAnimesBaseController com novos métodos
    Adiciona diretivas using para novos namespaces, incluindo DTOs, enumerações, solicitações e respostas. Implementa dois novos métodos assíncronos, `Ok<TData>(TData data)` e `Ok<TData>(IPagedEnumerable<TData> data, Pagination pagination)`, que retornam respostas padronizadas para operações bem-sucedidas. Os métodos verificam dados nulos e utilizam `DetailResponse<TData>` e `ListPagedResponse<TData>` para formatar as respostas, melhorando a clareza e usabilidade da API.

--- src/Giro.Animes.Presentation/Controllers/GiroAnimesBaseController.cs
+++ src/Giro.Animes.Presentation/Controllers/GiroAnimesBaseController.cs
@@ -1,6 +1,11 @@
-﻿using Giro.Animes.Application.Interfaces.Services.Base;
+﻿using Giro.Animes.Application.DTOs.Base;
+using Giro.Animes.Application.Interfaces.Enumerations;
+using Giro.Animes.Application.Interfaces.Services.Base;
+using Giro.Animes.Application.Requests;
+using Giro.Animes.Application.Responses;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Giro.Animes.Presentation.Controllers
 {
@@ -14,6 +19,37 @@ namespace Giro.Animes.Presentation.Controllers
         public GiroAnimesBaseController(TApplicationService applicationService)
         {
             _applicationService = applicationService;
+        }
+
+        public async Task<IActionResult> Ok<TData>(TData data) where TData : BaseDTO
+        {
+            if (data is null)
+            {
+                DetailResponse<TData> result = DetailResponse<TData>.Create(true, HttpStatusCode.OK, "Nenhum resultado encontrado");
+                return StatusCode((int)HttpStatusCode.OK, result);
+            }
+
+            DetailResponse<TData> response = DetailResponse<TData>.Create(data, true, HttpStatusCode.OK, "Consulta realizada com sucesso");
+            return await Task.Run(() =>
+            {
+                return StatusCode((int)HttpStatusCode.OK, response);
+            });
+        }
+
+        public async Task<IActionResult> Ok<TData>(IPagedEnumerable<TData> data, Pagination pagination) where TData : BaseDTO
+        {
+            if (data is null)
+            {
+                ListPagedResponse<TData> result = ListPagedResponse<TData>.Create(HttpStatusCode.OK, "A consulta não retornou resultados.", pagination.Page, pagination.RowsPerPage, data?.TotalCount ?? 0);
+                return StatusCode((int)HttpStatusCode.OK, result);
+            }
+
+            ListPagedResponse<TData> response = ListPagedResponse<TData>.Create(data, true, HttpStatusCode.OK, "Consulta realizada com sucesso", pagination.Page, pagination.RowsPerPage, data.TotalCount);
+
+            return await Task.Run(() =>
+            {
+                return StatusCode((int)HttpStatusCode.OK, response);
+            });
         }
     }
 }
