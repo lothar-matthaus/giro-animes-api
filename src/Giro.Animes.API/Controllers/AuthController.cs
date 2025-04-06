3e1b46348822eeb38d13688757f72eec94d8232e
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-06 00:22:25 -0300

    Adicionar AuthController para gerenciamento de autenticação
    Este commit adiciona uma nova classe `AuthController` ao namespace `Giro.Animes.API.Controllers`. A classe herda de `GiroAnimesBaseController<IAuthApplicationService>`, permitindo a integração com um serviço de aplicação de autenticação. O construtor da classe recebe um parâmetro do tipo `IAuthApplicationService` e o passa para o construtor da classe base. Também foram incluídos os diretórios `using` necessários para o serviço de aplicação e a classe base do controlador.

---
+++ src/Giro.Animes.API/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Giro.Animes.Application.Interfaces.Services;
+using Giro.Animes.Presentation.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Giro.Animes.API.Controllers
+{
+    public class AuthController : GiroAnimesBaseController<IAuthApplicationService>
+    {
+        public AuthController(IAuthApplicationService applicationService) : base(applicationService)
+        {
+        }
+    }
+}
