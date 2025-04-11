17df85ee381ae52897ef72c36370a04f8afe4c35
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-11 01:01:08 -0300

    Adiciona classe Policies e configurações de autorização
    Este commit introduz uma nova classe estática `Policies` em `Policies.cs`, que define constantes para permissões relacionadas a "Animes", "Authors" e "Account", incluindo ações como "CanGetAll" e "CanGetDetail".
    
    Além disso, em `ConfigurePolicies.cs`, são adicionadas políticas de autorização que utilizam as constantes definidas na classe `Policies`, configurando o acesso a visualizações de todos e detalhes para "Animes" e "Authors", exigindo uma reivindicação específica para autorização.

---
+++ src/Giro.Animes.Shared/Constants/Policies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Shared.Constants
+{
+    public static class Policies
+    {
+        public const string CLAIM_NAME = "Permission";
+        public static class Animes
+        {
+            public const string CAN_GET_ALL = "Animes:CanGetAll";
+            public const string CAN_GET_DETAIL = "Animes:CanGetDetail";
+        }
+
+        public static class Authors
+        {
+            public const string CAN_GET_ALL = "Authors:CanGetAll";
+            public const string CAN_GET_DETAIL = "Authors:CanGetDetail";
+        }
+
+        public static class  Account
+        {
+            public const string CAN_GET_ALL = "Account:CanGetAll";
+            public const string CAN_GET_DETAIL = "Account:CanGetDetail";
+        }
+    }
+}
---
+++ src/Giro.Animes.Shared/Extensions/Authorization/ConfigurePolicies.cs
@@ -0,0 +1,44 @@
+﻿using Giro.Animes.Domain.Enums;
+using Giro.Animes.Shared.Constants;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Shared.Extensions.Authorization
+{
+    public static class ConfigurePolicies
+    {
+        public static IServiceCollection AddAuthorizationWithPolicies(this IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(Policies.Animes.CAN_GET_ALL, policy =>
+                {
+                    policy.RequireClaim(Policies.CLAIM_NAME, Policies.Animes.CAN_GET_ALL);
+                });
+
+                options.AddPolicy(Policies.Animes.CAN_GET_DETAIL, policy =>
+                {
+                    policy.RequireClaim(Policies.CLAIM_NAME, Policies.Animes.CAN_GET_ALL);
+                });
+
+                // 
+                options.AddPolicy(Policies.Authors.CAN_GET_ALL, policy =>
+                {
+                    policy.RequireClaim(Policies.CLAIM_NAME, Policies.Authors.CAN_GET_ALL);
+                });
+
+                options.AddPolicy(Policies.Authors.CAN_GET_DETAIL, policy =>
+                {
+                    policy.RequireClaim(Policies.CLAIM_NAME, Policies.Authors.CAN_GET_DETAIL);
+                });
+            });
+
+            return services;
+        }
+    }
+}
