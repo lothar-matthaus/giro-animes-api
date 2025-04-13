27f6258ab03f738bdbce2382eabed57b3f0a712d
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-12 23:56:18 -0300

    Atualiza AccountsController para suporte a novas operações
    O arquivo `AccountsController.cs` foi modificado para incluir um novo DTO (`SimpleAccountDTO`) e alterar a forma como os detalhes da conta são recuperados e atualizados.
    O método `GetAccount` foi alterado para não aceitar mais um parâmetro `id`, em vez disso, ele recupera os detalhes da conta usando o ID do usuário do contexto HTTP.
    O método HTTP para atualizar contas foi alterado de `PUT` para `PATCH`, e o tipo de resposta foi atualizado para retornar um `DetailResponse<SimpleAccountDTO>`.
    Dois novos métodos foram adicionados: `UpdatePassword` e `UpdateSettings`, ambos utilizando o método `PATCH`. Esses métodos permitem atualizar a senha e as configurações do usuário, respectivamente, e incluem tipos de autorização e resposta apropriados.
    A estrutura geral do controlador foi aprimorada para suportar atualizações mais granulares das contas de usuário.

--- src/Giro.Animes.API/Controllers/AccountsController.cs
+++ src/Giro.Animes.API/Controllers/AccountsController.cs
@@ -1,5 +1,6 @@
 ﻿using Giro.Animes.Application.Constants;
 using Giro.Animes.Application.DTOs.Detailed;
+using Giro.Animes.Application.DTOs.Simple;
 using Giro.Animes.Application.Interfaces.Services;
 using Giro.Animes.Application.Requests.Account;
 using Giro.Animes.Application.Responses;
@@ -23,27 +24,48 @@ namespace Giro.Animes.API.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        [HttpGet("{id:long}")]
+        [HttpGet()]
         [Authorize(Policies.Account.CAN_GET_DETAIL)]
         [ProducesResponseType<DetailResponse<DetailedAccountDTO>>((int)HttpStatusCode.OK)]
         [ProducesResponseType<ErrorResponse>((int)HttpStatusCode.InternalServerError)]
         [ProducesResponseType<ApiResponse>((int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> GetAccount([FromRoute] long id)
+        public async Task<IActionResult> GetAccount()
         {
-            DetailedAccountDTO accountDTO = await _applicationService.GetAccountAndUserByAccountIdAsync(id);
+            DetailedAccountDTO accountDTO = await _applicationService.GetAccountAndUserByUserIdAsync(HttpContext.RequestAborted);
             return await Ok(accountDTO, Messages.Response.Account.ACCOUNT_FOUND, Messages.Response.Account.ACCOUNT_NOT_FOUND);
         }
 
-        [HttpPut()]
+        [HttpPatch()]
         [Authorize(Policies.Account.CAN_UPDATE)]
-        [ProducesResponseType<string>((int)HttpStatusCode.OK)]
+        [ProducesResponseType<DetailResponse<SimpleAccountDTO>>((int)HttpStatusCode.OK)]
         [ProducesResponseType<ErrorResponse>((int)HttpStatusCode.InternalServerError)]
         [ProducesResponseType<NotificationResponse>((int)HttpStatusCode.BadRequest)]
         public async Task<IActionResult> UpdateAccount([FromBody] AccountUpdateRequest request)
         {
-            // TODO: Implementar o método de atualização de conta
             await _applicationService.UpdateAccountAsync(request, HttpContext.RequestAborted);
             return StatusCode((int)HttpStatusCode.OK, Messages.Response.Account.ACCOUNT_UPDATED);
+        }
+
+        [HttpPatch("Password")]
+        [Authorize(Policies.Account.CAN_UPDATE)]
+        [ProducesResponseType<string>((int)HttpStatusCode.OK)]
+        [ProducesResponseType<ErrorResponse>((int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType<NotificationResponse>((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> UpdatePassword([FromBody] AccountPasswordUpdateRequest request)
+        {
+            await _applicationService.UpdatePasswordAsync(request, HttpContext.RequestAborted);
+            return StatusCode((int)HttpStatusCode.OK, Messages.Response.Account.PASSWORD_UPDATED);
+        }
+
+        [HttpPatch("Settings")]
+        [Authorize(Policies.Account.CAN_UPDATE)]
+        [ProducesResponseType<DetailResponse<SimpleSettingsDTO>>((int)HttpStatusCode.OK)]
+        [ProducesResponseType<ErrorResponse>((int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType<NotificationResponse>((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> UpdateSettings([FromBody] AccountSettingsUpdateRequest request)
+        {
+            SimpleSettingsDTO simpleSettingsDTO = await _applicationService.UpdateSettingsAsync(request, HttpContext.RequestAborted);
+            return await Ok(simpleSettingsDTO, Messages.Response.Account.SETTINGS_UPDATED, Messages.Response.Account.SETTINGS_NOT_UPDATED);
         }
     }
 }
