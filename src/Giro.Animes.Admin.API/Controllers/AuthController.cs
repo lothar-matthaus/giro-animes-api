f072aacb6ed83b7266f9f7b393a786ec181b60d2
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-14 20:14:10 -0300

    Atualiza AuthController com autenticação e novos métodos
    O arquivo `AuthController.cs` foi modificado para incluir novos namespaces necessários. Uma nova classe `AuthController` foi criada, herdando de `GiroAnimesBaseController<IAuthService>`, e um campo privado `_accountService` foi adicionado. O construtor agora aceita um parâmetro `IAccountService`. Um método assíncrono `Auth` foi introduzido, permitindo acesso anônimo, que processa uma solicitação de autenticação e retorna uma resposta com o resultado da autenticação.

---
+++ src/Giro.Animes.Admin.API/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Giro.Animes.Application.Constants;
+using Giro.Animes.Application.DTOs;
+using Giro.Animes.Application.Interfaces.Services;
+using Giro.Animes.Application.Requests.Auth;
+using Giro.Animes.Application.Responses;
+using Giro.Animes.Presentation.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+
+namespace Giro.Animes.API.Controllers
+{
+    public class AuthController : GiroAnimesBaseController<IAuthService>
+    {
+        private readonly IAccountService _accountService;
+
+        public AuthController(IAuthService applicationService, IAccountService accountService) : base(applicationService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost()]
+        [AllowAnonymous]
+        [ProducesResponseType<DetailResponse<AuthDTO>>((int)HttpStatusCode.OK)]
+        [ProducesResponseType<ErrorResponse>((int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType<NotificationResponse>((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Auth([FromForm] AuthRequest request)
+        {
+            AuthDTO authDTO = await _applicationService.AuthAdminAsync(request, HttpContext.RequestAborted);
+            return await Ok(authDTO, Messages.Response.Auth.AUTHENTICATION_SUCCESS, Messages.Response.Auth.AUTHENTICATION_FAILED);
+        }
+    }
+}
