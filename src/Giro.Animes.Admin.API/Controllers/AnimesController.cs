7e63cf2b74c483c6d970fa301e6f5884726c1880
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-14 19:23:35 -0300

    Atualiza API de Animes e remove arquivos obsoletos
    - Adiciona o controlador `AnimesController` para gerenciar requisições relacionadas a animes, incluindo métodos para busca paginada, busca por ID e criação de animes.
    - Atualiza o arquivo `.gitignore` para incluir `MigrationBackup/` e `appsettings.Development.json` do projeto `Giro.Animes.Admin.API`.
    - Remove completamente o controlador `WeatherForecastController` e o arquivo `WeatherForecast.cs`.
    - Modifica o arquivo `Giro.Animes.Admin.API.csproj` para incluir referências a `Giro.Animes.Application` e `Giro.Animes.Shared`.
    - Atualiza `Program.cs` com novas diretivas `using` e configurações de serviços, incluindo filtros e middleware, além de alterar a configuração do Swagger.
    - Altera `launchSettings.json` para não abrir o navegador automaticamente.
    - Remove o arquivo `appsettings.Development.json`, que continha configurações de log.

---
+++ src/Giro.Animes.Admin.API/Controllers/AnimesController.cs
@@ -0,0 +1,65 @@
+﻿using Giro.Animes.Application.Constants;
+using Giro.Animes.Application.DTOs.Detailed;
+using Giro.Animes.Application.DTOs.Simple;
+using Giro.Animes.Application.Interfaces.Enumerations;
+using Giro.Animes.Application.Interfaces.Services;
+using Giro.Animes.Application.Requests;
+using Giro.Animes.Application.Requests.Anime;
+using Giro.Animes.Application.Responses;
+using Giro.Animes.Domain.Common.Filters;
+using Giro.Animes.Presentation.Controllers;
+using Giro.Animes.Shared.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Giro.Animes.API.Controllers
+{
+    public class AnimesController : GiroAnimesBaseController<IAnimeService>
+    {
+        public AnimesController(IAnimeService applicationService) : base(applicationService)
+        {
+        }
+
+        /// <summary>
+        /// Busca todos os animes paginados, dado filtros específicos
+        /// </summary>
+        /// <param name="pagination"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType<IPagedEnumerable<SimpleAnimeDTO>>((int)HttpStatusCode.OK)]
+        [ProducesResponseType<ErrorResponse>((int)HttpStatusCode.InternalServerError)]
+        [Authorize(Policy = Policies.Animes.CAN_GET_ALL)]
+        public async Task<IActionResult> GetAllPaged([FromQuery] Pagination<AnimeFilter> pagination)
+        {
+            IPagedEnumerable<SimpleAnimeDTO> pagedResult = await _applicationService.GetAllPagedAsync(pagination, HttpContext.RequestAborted);
+            return await Ok(pagedResult, pagination, Messages.Response.Anime.ANIMES_FOUND, Messages.Response.Anime.ANIMES_NOT_FOUND);
+        }
+
+        /// <summary>
+        /// Busca um anime pelo id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id:long}")]
+        [ProducesResponseType<DetailedAnimeDTO>((int)HttpStatusCode.OK)]
+        [ProducesResponseType<ErrorResponse>((int)HttpStatusCode.InternalServerError)]
+        [Authorize(Policy = Policies.Animes.CAN_GET_DETAIL)]
+        public async Task<IActionResult> Get([FromRoute] long id)
+        {
+            DetailedAnimeDTO anime = await _applicationService.GetByIdAsync(id, HttpContext.Request.HttpContext.RequestAborted);
+            return await Ok(anime, Messages.Response.Anime.ANIME_FOUND, Messages.Response.Anime.ANIME_NOT_FOUND);
+        }
+
+        [HttpPost()]
+        [ProducesResponseType<DetailedAnimeDTO>((int)HttpStatusCode.Created)]
+        [ProducesResponseType<ErrorResponse>((int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType<NotificationResponse>((int)HttpStatusCode.BadRequest)]
+        [Authorize(Policy = Policies.Animes.CAN_CREATE)]
+        public async Task<IActionResult> CreateAnime([FromForm] AnimeCreateRequest request)
+        {
+            DetailedAnimeDTO detailedAnimeDTO = await _applicationService.CreateAnimeAsync(request, HttpContext.RequestAborted);
+            return await Ok(detailedAnimeDTO, Messages.Response.Anime.ANIME_CREATED);
+        }
+    }
+}
--- src/Giro.Animes.Admin.API/Program.cs
+++ src/Giro.Animes.Admin.API/Program.cs
@@ -1,25 +1,77 @@
+using Giro.Animes.Application.Extensions.IoC;
+using Giro.Animes.Infra.Data.Extensions.IoC;
+using Giro.Animes.Infra.Extensions;
+using Giro.Animes.Infra.Extensions.IoC;
+using Giro.Animes.Infra.Extensions.IoC.Security;
+using Giro.Animes.Shared.Extensions.Authorization;
+using Giro.Animes.Shared.Extensions.Swaggger;
+using Giro.Animes.Shared.Filters;
+using Giro.Animes.Shared.Middleware;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Configura os serviços da API (Controllers, etc.)
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<NotificationResultFilter>();
+    options.Filters.Add<SaveChangesResultFilter>();
+}).AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.WriteIndented = true;
+});
+
+// Configura o acesso ao contexto HTTP
+builder.Services.AddHttpContextAccessor();
+
+// Adiciona as configurações da aplicação
+builder.Services.AddApplicationUser();
+builder.Services.AddAppConfig();
+
+// Configura o usuário do contexto da requisição
+// Configura o AutoMapper
+builder.Services.AddSwaggerConfig(builder.Configuration);
+
+// Configura a autenticação JWT
+builder.Services.AddJwtAuthorization(builder.Configuration);
+
+builder.Services.AddAuthorizationWithPolicies();
+#region Contexts
+builder.Services.AddGiroAnimesDbContext();
+#endregion
+
+#region Repositories
+builder.Services.AddRepositories();
+#endregion
+
+#region Services
+builder.Services.ConfigureDomainServices();
+builder.Services.ConfigureApplicationServices();
+builder.Services.AddServices();
+#endregion
+
+#region Middlewares
+builder.Services.AddTransient<ExceptionHandlingMiddleware>();
+#endregion
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerConfig();
 }
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseRouting();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
