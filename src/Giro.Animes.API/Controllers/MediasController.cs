d017fa3f6c3146b13b53dc7c120ba6f8b12312a5
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-16 22:38:13 -0300

    Adicionar MediasController para operações de mídia
    Este commit introduz a nova classe `MediasController` no namespace `Giro.Animes.Shared.Controllers`, que herda de `GiroAnimesBaseController<IMediaService>`. A classe é projetada para gerenciar operações relacionadas a mídia, utilizando a interface de serviço de mídia especificada. O construtor aceita uma instância de `IMediaService` e a passa para o construtor da classe base. Além disso, um novo método de ação assíncrono chamado `Download` foi adicionado, permitindo acesso anônimo. Este método recebe um `token` como parâmetro de consulta e um `CancellationToken`, recupera um objeto `FileDTO` do serviço de mídia e retorna o arquivo junto com seu tipo de conteúdo.

---
+++ src/Giro.Animes.API/Controllers/MediasController.cs
@@ -0,0 +1,23 @@
+﻿using Giro.Animes.Application.DTOs;
+using Giro.Animes.Application.Interfaces.Services;
+using Giro.Animes.Presentation.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Giro.Animes.Shared.Controllers
+{
+    public class MediasController : GiroAnimesBaseController<IMediaService>
+    {
+        public MediasController(IMediaService applicationService) : base(applicationService)
+        {
+        }
+
+        [HttpGet("Download")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Download([FromQuery]string token, CancellationToken cancellationToken)
+        {
+            FileDTO media = await _applicationService.DownloadAsync(token, cancellationToken);
+            return File(media.File, media.ContentType, true);
+        }
+    }
+}
