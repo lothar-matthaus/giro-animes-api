6828ebcb2bd952fdd18d36836f19e62759f269f1
Lothar Matthaus <lothar1258@hotmail.com>
2025-04-19 19:34:48 -0300

    Adicionar classe EmailTemplate e enum TemplateType
    Este commit introduz a nova classe `EmailTemplate` no namespace `Giro.Animes.Domain.Entities`, que encapsula propriedades relacionadas a templates de e-mail, como `Subject`, `Body`, `TemplateType`, `TemplateName`, `TemplateDescription` e `Language`. A classe possui um construtor privado que força a criação de objetos através de um método estático `Create`.
    
    Além disso, um novo enum `TemplateType` foi adicionado no namespace `Giro.Animes.Domain.Enums`, definindo vários tipos de templates de e-mail, como `Welcome`, `PasswordReset`, `AccountVerification`, entre outros, proporcionando uma maneira estruturada de categorizar diferentes templates de e-mail utilizados na aplicação.

---
+++ src/Giro.Animes.Domain/Entities/EmailTemplate.cs
@@ -0,0 +1,53 @@
+﻿using Giro.Animes.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Domain.Entities
+{
+    public class EmailTemplate : EntityBase
+    {
+        public string Subject { get; private set; }
+        public string Body { get; private set; }
+        public string TemplateType { get; private set; }
+        public string TemplateName { get; private set; }
+        public string TemplateDescription { get; private set; }
+
+        public long LanguageId { get; private set; }
+        public Language Language { get; private set; }
+
+        /// <summary>
+        /// Construtor privado com parâmetros. Garanta a construção do objeto através do método de Create.
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        /// <param name="templateType"></param>
+        /// <param name="templateName"></param>
+        /// <param name="templateDescription"></param>
+        private EmailTemplate (string subject, string body, string templateType, string templateName, string templateDescription, Language language)
+        {
+            Subject = subject;
+            Body = body;
+            TemplateType = templateType;
+            TemplateName = templateName;
+            TemplateDescription = templateDescription;
+            Language = language;
+        }
+
+        /// <summary>
+        /// Método de criação do objeto EmailTemplate.
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        /// <param name="templateType"></param>
+        /// <param name="templateName"></param>
+        /// <param name="templateDescription"></param>
+        /// <returns></returns>
+        public static EmailTemplate Create(string subject, string body, string templateType, string templateName, string templateDescription, Language language)
+        {
+            return new EmailTemplate(subject, body, templateType, templateName, templateDescription, language);
+        }
+    }
+}
---
+++ src/Giro.Animes.Domain/Enums/TemplateType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giro.Animes.Domain.Enums
+{
+    public enum TemplateType
+    {
+        None = 0,
+        Welcome = 1,
+        PasswordReset = 2,
+        AccountVerification = 3,
+        PasswordChange = 4,
+        AccountDeactivation = 5,
+        AccountReactivation = 6,
+        SubscriptionConfirmation = 7,
+        SubscriptionCancellation = 8,
+        SubscriptionRenewal = 9,
+        SubscriptionUpgrade = 10,
+        SubscriptionDowngrade = 11,
+        WatchlistAnimeUpdated = 12
+    }
+}
